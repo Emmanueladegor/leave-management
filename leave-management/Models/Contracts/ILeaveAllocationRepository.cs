c1dd7336d867d1579b429b7ab8b4b6e19a329d8c
emmanueladegor <iroromeh1*>
2021-02-06 14:38:17 +0100

    Initial commit for Data Modelling

---
+++ leave-management/Models/Contracts/ILeaveAllocationRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using leave_management.Data;
+
+namespace leave_management.Models.Contracts
+{
+    public interface ILeaveAllocationRepository : IRepositoryBase<LeaveAllocation>
+    {
+    }
+}
---
+++ leave-management/Models/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using leave_management.Data;
+
+namespace leave_management.Models.Contracts
+{
+    public interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    {
+        ICollection<LeaveType> GetEmployeeByLeaveType(int id);
+    }
+}
